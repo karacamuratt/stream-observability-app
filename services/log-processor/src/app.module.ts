21433e5c7515f2d52ccc5a8230967639885eed2b
Murat Karaca <murat__karaca@windowslive.com>
2026-02-26 18:25:31 +0300

    updated render deployment changes and fixed deployment errors.

--- services/log-processor/src/app.module.ts
+++ services/log-processor/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { KafkaModule } from './kafka/kafka.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
     imports: [KafkaModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule { }
